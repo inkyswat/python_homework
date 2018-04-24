9cb0d80c849cc992270d8af96c6eb3f905ccbad9
inkyswat <inkyswat@tlu.ee>
2018-04-25 02:46:07 +0300

    tick-tack-toe edasi arendused

--- tick-tack-toe.py
+++ tick-tack-toe.py
@@ -2,9 +2,15 @@
 import subprocess # subprocess võimaldab antud kontekstis os'ile käske saata
 # 'import os' oleks alternatiiv, kuid try ei suuda seda errorit kinni püüda
 
-sisend = ""
-roll = False
-valik = [[],[],[]]
+alustaja_valitud = False
+roll = ""
+hetke_seis = [[0,0,0],[0,0,0],[0,0,0]]
+rida = ""
+veerg = ""
+teade = ""
+veateade = ""
+m2ng_l2bi = False
+sisu_test = True
 # ================
 # FUNKTISOONID
 # ================
@@ -16,46 +22,109 @@ def tyhjenda_ekraan():
 
 
 def tyhi_tabel():
+    global teade
     print("")
     print("           A       B       C")
     print("")
-    print("       =========================")
+    print("       -------------------------")
     print("       |       |       |       |")
     print("   1   |       |       |       |")
     print("       |       |       |       |")
-    print("       =========================")
+    print("       -------------------------")
     print("       |       |       |       |")
     print("   2   |       |       |       |")
     print("       |       |       |       |")
-    print("       =========================")
+    print("       -------------------------")
     print("       |       |       |       |")
     print("   3   |       |       |       |")
     print("       |       |       |       |")
-    print("       =========================")
-    print("")
+    print("       -------------------------")
+    print(teade)
+    print(veateade)
+    print("________________________________")
 
-def ring_voi_x(sisend):
-    print()
 
-def kysi_sisendeid():
-    global roll # global variable'i accessimine, selleks on vaja fn'i sees global def'da
-    if (roll):
-        global valik
-        valik[1] = input("Sisesta palun rida (1 - 3) ")
-        valik[2] = input("Sisesta palun veerg (A - C) ")
+def kysi_koordinaadid():
+    global rida
+    global veerg
+    global veateade
+    korrektsed_koordinaadid = False
+    while (not korrektsed_koordinaadid):
+        tyhjenda_ekraan()
+        tyhi_tabel()
+
+        rida = input("Sisesta palun rida (1 - 3) ")
+        try:
+            rida = int(rida)
+            rida -= 1  # et array index oleks õige vähendame rida
+            veerg = input("ja nüüd palun veerg (a - c) ")
+            veerg = veerg.lower()
+            if ((rida >= 0 and rida <= 2) and (veerg == "a" or veerg == "b" or veerg == "c")):
+                if (veerg == "a"):
+                    veerg = 0
+                if (veerg == "b"):
+                    veerg = 1
+                if (veerg == "c"):
+                    veerg = 2
+
+                korrektsed_koordinaadid = True
+                veateade = ""
+            else:
+                korrektsed_koordinaadid = False  
+                veateade = f"mängija \'{roll}\' poolt valesti sisestatud koordinaadid"
+        except ValueError:
+            korrektsed_koordinaadid = False
+            veateade = f"mängija \'{roll}\' poolt valesti sisestatud koordinaadid"
+
+
+def kysi_roll():
+    global roll
+    global alustaja_valitud
+    while (not alustaja_valitud):
+        tyhjenda_ekraan()
+        tyhi_tabel()
+        roll = input("Kumb enne, kas \'o\' või \'x\'? ")
+        if (roll == "o" or roll == "x" or roll == "O" or roll == "X"):
+            roll = roll.lower()
+            alustaja_valitud = True
+        else:
+            alustaja_valitud = False
+
+def sisu_kontroll(rida, veerg):
+    global hetke_seis
+    if (hetke_seis[rida][veerg] == 0):
+        return True
     else:
-        sisend = input("kes alustab, kas O või X ?")
-        roll = True
+        return False    
+
+def sisu_t2itmine(rida, veerg, _roll):
+    global hetke_seis
+    hetke_seis[rida][veerg] = _roll
     
-
-def arvuta_tabel(hetke_seis):
-    print()
-
-
-
+def rolli_muutmine(_roll):
+    global roll
+    global teade
+    if (_roll == "x"):
+        roll = "o"
+        teade = f"Preagu on mängija \'{roll}\' kord"
+    else:
+        roll = "x"
+        teade = f"Preagu on mängija \'{roll}\' kord"
+# ==========================
+# Start
+# ==========================
 tyhjenda_ekraan()
 tyhi_tabel()
-kysi_sisendeid()
 
-# hetke_seis = [[0,1,0],[1,0,1],[0,1,0]]
-# print (hetke_seis[1][2])
+kysi_roll()
+teade = f"Preagu on mängija \'{roll}\' kord"
+for x in range(4):
+    kysi_koordinaadid()
+    sisu_seis_ok = sisu_kontroll(rida, veerg)
+    while (not sisu_seis_ok):
+        veateade = "Seal on juba olemas"
+        kysi_koordinaadid()
+        sisu_seis_ok = sisu_kontroll(rida, veerg)
+    sisu_t2itmine(rida, veerg, roll)
+    rolli_muutmine(roll)
+print(hetke_seis)
