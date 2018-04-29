9e996dfb89542790c888b0234b5ec2d04154cb5d
inkyswat <inkyswat@tlu.ee>
2018-04-29 20:28:49 +0300

    kastidesse tulemuste kuvamine

--- tick-tack-toe.py
+++ tick-tack-toe.py
@@ -1,29 +1,17 @@
+import os  # for os operations
+import platform # os platform tools
 
-
-import os  # võimaldab os'ile käske saata
-import platform # os'i platformi vahendid
-platform = platform.system()
-color_os = 0
-RolePicked = False
-role = ""
-CurState = [[0,0,0],[0,0,0],[0,0,0]]
-row = ""
-column = ""
-Notific = ""
-ErrorMsg = ""
-GameOver = False
-sisu_test = True
-
-# ================
-# FUNKTISOONID
-# ================
+# ====================
+# Colors
+# ====================
 class Colors:
-    CEND      = ['\33[0m',''] # värvi lõpetamiseks (näiteks: vilkumine))
+    CEND      = ['\33[0m',''] # <- for ending color or blinking
+
     CBOLD     = ['\33[1m', '']
     CITALIC   = ['\33[3m', '']
     CURL      = ['\33[4m', '']
-    CBLINK    = ['\33[5m', '']
-    CBLINK2   = ['\33[6m', '']
+    CBLINK    = ['\33[5m', ''] # <- blinking
+    CBLINK2   = ['\33[6m', ''] # <- blinking
     CSELECTED = ['\33[7m', '']
 
     CBLACK    = ['\33[30m', '']
@@ -34,15 +22,6 @@ class Colors:
     CVIOLET   = ['\33[35m', '']
     CBEIGE    = ['\33[36m', '']
     CWHITE    = ['\33[37m', '']
-# background colors
-    CBLACKBG  = ['\33[40m', '']
-    CREDBG    = ['\33[41m', '']
-    CGREENBG  = ['\33[42m', '']
-    CYELLOWBG = ['\33[43m', '']
-    CBLUEBG   = ['\33[44m', '']
-    CVIOLETBG = ['\33[45m', '']
-    CBEIGEBG  = ['\33[46m', '']
-    CWHITEBG  = ['\33[47m', '']
 
     CGREY     = ['\33[90m', '']
     CRED2     = ['\33[91m', '']
@@ -53,6 +32,16 @@ class Colors:
     CBEIGE2   = ['\33[96m', '']
     CWHITE2   = ['\33[97m', '']
 
+# background colors
+    CBLACKBG  = ['\33[40m', '']
+    CREDBG    = ['\33[41m', '']
+    CGREENBG  = ['\33[42m', '']
+    CYELLOWBG = ['\33[43m', '']
+    CBLUEBG   = ['\33[44m', '']
+    CVIOLETBG = ['\33[45m', '']
+    CBEIGEBG  = ['\33[46m', '']
+    CWHITEBG  = ['\33[47m', '']
+
     CGREYBG    = ['\33[100m', '']
     CREDBG2    = ['\33[101m', '']
     CGREENBG2  = ['\33[102m', '']
@@ -62,34 +51,80 @@ class Colors:
     CBEIGEBG2  = ['\33[106m', '']
     CWHITEBG2  = ['\33[107m', '']
 
-def platform_ja_Colors():
-    global platform
-    if (platform == "Windows"):
-        print()
+
+# -------------------------------------------
+# variables
+# -------------------------------------------
+platform = platform.system()
+color_os = 0
+RolePicked = False
+role = ""
+WriteData = 0
+CurState = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+row = ""
+column = ""
+Notific = ""
+ErrorMsg = ""
+GameOver = False
+WinnerAdder = ""
+WinnerObj = ""
+# colors
+tableColor = Colors.CBEIGE[color_os]
+disp_X_color = Colors.CBLACK[color_os] + Colors.CGREYBG[color_os]
+disp_O_color = Colors.CBLACK[color_os] + Colors.CYELLOWBG[color_os]
+ErrorMsgCol = Colors.CWHITE[color_os]
+
+
+# Table Stuff
+dispRow_separator = tableColor + "        =======================" + Colors.CEND[color_os]
+dispEmptyCell = tableColor + "       |" + Colors.CEND[color_os]
+
+disp_X_Line_1 = disp_X_color + "  \ /  " + Colors.CEND[color_os] + tableColor + "|" + Colors.CEND[color_os]
+disp_X_Line_2 = disp_X_color + "   x   " + Colors.CEND[color_os] + tableColor + "|" + Colors.CEND[color_os]
+disp_X_Line_3 = disp_X_color + "  / \  " + Colors.CEND[color_os] + tableColor + "|" + Colors.CEND[color_os]
+
+disp_O_Line_1 = disp_O_color + " /---\ " + Colors.CEND[color_os] + tableColor + "|" + Colors.CEND[color_os]
+disp_O_Line_2 = disp_O_color + "(CRCLE)" + Colors.CEND[color_os] + tableColor + "|" + Colors.CEND[color_os]
+disp_O_Line_3 = disp_O_color + " \---/ " + Colors.CEND[color_os] + tableColor + "|" + Colors.CEND[color_os]
+
+dispRow_1_number = tableColor + "   1   |" + Colors.CEND[color_os]
+dispRow_2_number = tableColor + "   2   |" + Colors.CEND[color_os]
+dispRow_3_number = tableColor + "   3   |" + Colors.CEND[color_os]
+
+dispRows = [[["0", "0", "0", "0"], ["0", "0", "0", "0"], ["0", "0", "0", "0"]], [["0", "0", "0", "0"], ["0", "0", "0", "0"], ["0", "0", "0", "0"]], [["0", "0", "0", "0"], ["0", "0", "0", "0"], ["0", "0", "0", "0"]]]
+
+
+# ====================
+# Functions
+# ====================
+
 def ClearScreen():
 	os.system('cls' if os.name=='nt' else 'clear')
+# -------------------------------------------
 
-def DisplayTable(): # pane siia sisend ja kontroll kuhu tulemus kirjutada
+def DisplayTable():
     global Notific
-    print(Colors.CBEIGE[color_os])
+    global ErrorMsg
+    print(tableColor)
     print("           A       B       C")
     print("")
-    print("       -------------------------")
-    print("       |       |       |       |")
-    print("   1   |       |       |       |")
-    print("       |       |       |       |")
-    print("       -------------------------")
-    print("       |       |       |       |")
-    print("   2   |       |       |       |")
-    print("       |       |       |       |")
-    print("       -------------------------")
-    print("       |       |       |       |")
-    print("   3   |       |       |       |")
-    print("       |       |       |       |")
-    print("       -------------------------")
+    print(dispRow_separator)
+    print(dispRows[0][0][0]+dispRows[0][0][1]+dispRows[0][0][2]+dispRows[0][0][3])
+    print(dispRows[0][1][0]+dispRows[0][1][1]+dispRows[0][1][2]+dispRows[0][1][3])
+    print(dispRows[0][2][0]+dispRows[0][2][1]+dispRows[0][2][2]+dispRows[0][2][3])
+    print(dispRow_separator)
+    print(dispRows[1][0][0]+dispRows[1][0][1]+dispRows[1][0][2]+dispRows[1][0][3])
+    print(dispRows[1][1][0]+dispRows[1][1][1]+dispRows[1][1][2]+dispRows[1][1][3])
+    print(dispRows[1][2][0]+dispRows[1][2][1]+dispRows[1][2][2]+dispRows[1][2][3])
+    print(dispRow_separator)
+    print(dispRows[2][0][0]+dispRows[2][0][1]+dispRows[2][0][2]+dispRows[2][0][3])
+    print(dispRows[2][1][0]+dispRows[2][1][1]+dispRows[2][1][2]+dispRows[2][1][3])
+    print(dispRows[2][2][0]+dispRows[2][2][1]+dispRows[2][2][2]+dispRows[2][2][3])
+    print(dispRow_separator)
     print(Notific)
     print(ErrorMsg)
     print(Colors.CGREY[color_os] + "________________________________")
+# -------------------------------------------
 
 
 def AskCoordinates():
@@ -101,11 +136,11 @@ def AskCoordinates():
         ClearScreen()
         DisplayTable()
 
-        row = input(Colors.CBOLD[color_os] + Colors.CGREY[color_os] + "Sisesta palun row (1 - 3) " + Colors.CEND[color_os])
+        row = input(Colors.CBOLD[color_os] + Colors.CGREY[color_os] + "      Please enter row (1 - 3) " + Colors.CEND[color_os])
         try:
             row = int(row)
-            row -= 1  # et array index oleks õige vähendame row
-            column = input(Colors.CBOLD[color_os] + Colors.CGREY[color_os] + "ja nüüd palun column (a - c) " + Colors.CEND[color_os])
+            row -= 1  # <- for correct array index we'll to -1
+            column = input(Colors.CBOLD[color_os] + Colors.CGREY[color_os] + "  ... and now column please (a - c) " + Colors.CEND[color_os])
             column = column.lower()
             if ((row >= 0 and row <= 2) and (column == "a" or column == "b" or column == "c")):
                 if (column == "a"):
@@ -119,52 +154,131 @@ def AskCoordinates():
                 ErrorMsg = ""
             else:
                 Coordin_ok = False  
-                ErrorMsg = Colors.CBLINK[color_os] + Colors.CRED2[color_os] + f"mängija {role} poolt valesti sisestatud koordinaadid" + Colors.CEND[color_os]
+                ErrorMsg = Colors.CBLINK[color_os] + Colors.CRED2[color_os] + f"Incorrect parameters given by player {role}" + Colors.CEND[color_os]
         except ValueError:
             Coordin_ok = False
-            ErrorMsg = Colors.CBLINK[color_os] + Colors.CRED2[color_os] + f"mängija {role} poolt valesti sisestatud koordinaadid" + Colors.CEND[color_os]
+            ErrorMsg = Colors.CBLINK[color_os] + Colors.CRED2[color_os] + f"Incorrect parameters given by player {role}" + Colors.CEND[color_os]
+# -------------------------------------------
 
 
 def AskRole():
     global role
     global RolePicked
     global Notific
+    global WriteData
     while (not RolePicked):
         ClearScreen()
         DisplayTable()
-        role = input(Colors.CGREEN2[color_os] + "Kumb enne, kas \'o\' või \'x\'? " + Colors.CEND[color_os])
+        role = input(Colors.CWHITE[color_os] + "      Who goes first \'o\' or \'x\'? " + Colors.CEND[color_os])
         if (role == "o" or role == "x" or role == "O" or role == "X"):
             role = role.lower()
             RolePicked = True
             if (role == "x"):
-                Notific = Colors.CVIOLET[color_os] + "Preagu on mängija " + Colors.CURL[color_os] + Colors.CGREY[color_os] + f"{role}" + Colors.CEND[color_os] + Colors.CVIOLET[color_os] + " kord" + Colors.CEND[color_os]
+                Notific = Colors.CVIOLET[color_os] + "It is your turn " + Colors.CBOLD[color_os] + Colors.CGREY[color_os] +  Colors.CURL[color_os] + f"{role}" + Colors.CEND[color_os]
+                WriteData = 1
             else:
-                Notific = Colors.CVIOLET[color_os] + "Preagu on mängija " + Colors.CGREEN[color_os] + Colors.CURL[color_os] + f"{role}" + Colors.CEND[color_os] + Colors.CVIOLET[color_os] + " kord" + Colors.CEND[color_os]
+                Notific = Colors.CVIOLET[color_os] + "It is your turn " + Colors.CBOLD[color_os] + Colors.CYELLOW[color_os] + Colors.CURL[color_os] + f"{role}" + Colors.CEND[color_os]
+                WriteData = -1 # <- for 'o' is -1
         else:
             RolePicked = False
+# -------------------------------------------
 
-def sisu_kontrole(row, column):
+
+def Chck_if_exists(row, column):
     global CurState
     if (CurState[row][column] == 0):
         return True
     else:
         return False    
+# -------------------------------------------
+
 
 def FillArray(row, column, _role):
     global CurState
     CurState[row][column] = _role
-    
+# -------------------------------------------
+
+
 def ChangeRole(_role):
     global role
     global Notific
+    global WriteData
     if (_role == "x"):
         role = "o"
-        Notific = Colors.CVIOLET[color_os] + "Preagu on mängija " + Colors.CGREEN[color_os] + Colors.CURL[color_os] + f"{role}" + Colors.CEND[color_os] + Colors.CVIOLET[color_os] + " kord" + Colors.CEND[color_os]
+        WriteData = -1
+        Notific = Colors.CVIOLET[color_os] + "It is your turn " + Colors.CBOLD[color_os] + Colors.CYELLOW[color_os] + Colors.CURL[color_os] + f"{role}" + Colors.CEND[color_os]
     else:
         role = "x"
-        Notific = Colors.CVIOLET[color_os] + "Preagu on mängija " + Colors.CURL[color_os] + Colors.CGREY[color_os] + f"{role}" + Colors.CEND[color_os] + Colors.CVIOLET[color_os] + " kord" + Colors.CEND[color_os]
-# massiivist mitme elemedi korraga accessimine
-# massiiv[1:3] => ühest kolmeni elemendid
+        WriteData = 1
+        Notific = Colors.CVIOLET[color_os] + "It is your turn " + Colors.CBOLD[color_os] + Colors.CGREY[color_os] + Colors.CURL[color_os] + f"{role}" + Colors.CEND[color_os]
+
+# -------------------------------------------
+def CalcDispArrays():
+    global CurState
+    for row in range(0, 3):
+        for cell in range(0, 3):
+            if(CurState[row][cell] == -1):
+                dispRows[row][0][cell+1] = disp_O_Line_1
+                dispRows[row][1][cell+1] = disp_O_Line_2
+                dispRows[row][2][cell+1] = disp_O_Line_3
+            if(CurState[row][cell] == 1):
+                dispRows[row][0][cell+1] = disp_X_Line_1
+                dispRows[row][1][cell+1] = disp_X_Line_2
+                dispRows[row][2][cell+1] = disp_X_Line_3
+
+# -------------------------------------------
+
+def TableInit():
+    for row in range(0, 3):
+        for line in range(0, 3):
+            for cell in range(0, 4):
+                if ((row == 0 and line == 1 and cell == 0) or (row == 1 and line == 1 and cell == 0) or (row == 2 and line == 1 and cell == 0)):
+                    dispRows[0][1][0] = dispRow_1_number
+                    dispRows[1][1][0] = dispRow_2_number
+                    dispRows[2][1][0] = dispRow_3_number
+                else:
+                    dispRows[row][line][cell] = dispEmptyCell
+# -------------------------------------------
+
+class WinnerCheck:
+    global CurState
+    global WinnerAdder
+    TempVar = ""
+    def Calc(TestCounter):
+        if (TestMode == 0):
+            for line in range(3):
+                WinnerAdder = CurState[line][0] + CurState[line][1] + CurState[line][2]
+                if (WinnerAdder == 3):
+                    return 3
+                if (WinnerAdder == -3):
+                    return -3
+            else:
+                return    
+        if (TestMode == 1):
+            print()
+        if (TestMode == 2):
+            print()
+
+    def test():
+        for TestMode in range(3):
+            TempVar = self.Calc(TestMode)
+
+            if(TempVar == 3):
+                return 1 # < if winner is x
+            if(TempVar == -3):
+                return -1 # < if winner is o 
+        print()
+  #  if ((CurState[0][0] + CurState[0][1] + CurState[0][2] == 3) or )
+     
+# -------------------------------------------
+
+def MarkWinnerCells():
+    print()
+
+# ===========================================
+# ===========================================
+
+
 
 # ==========================
 # Start
@@ -173,20 +287,26 @@ if (platform == "Windows"):
     color_os = 1
 else:
     color_os = 0  
-# print(Colors.CGREEN2[color_os])
-# while True:
-#     print      
+
 ClearScreen()
+TableInit()
 DisplayTable()
 
 AskRole()
-for x in range(2):
+for x in range(9):
     AskCoordinates()
-    sisu_seis_ok = sisu_kontrole(row, column)
-    while (not sisu_seis_ok):
-        ErrorMsg = Colors.CWHITE2[color_os] + "Seal on juba olemas"
-        AskCoordinates()
-        sisu_seis_ok = sisu_kontrole(row, column)
-    FillArray(row, column, role)
-    ChangeRole(role)
+    Content_ok = Chck_if_exists(row, column)
+    if (not GameOver):
+        while (not Content_ok):
+            ErrorMsg = ErrorMsgCol + "Already filled!" + Colors.CEND[color_os]
+            AskCoordinates()
+            Content_ok = Chck_if_exists(row, column)
+        FillArray(row, column, WriteData)
+        ChangeRole(role)
+        CalcDispArrays()
+        ClearScreen()
+        DisplayTable()
+    else:
+        MarkWinnerCells()
+        DisplayTable()
 print(CurState)
